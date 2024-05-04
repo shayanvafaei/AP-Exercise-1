81a5a1dec48c9591eefec2607cf2c82bab1d9956
Saba Madadi <sabamadadi9@gmail.com>
2024-05-04 14:30:28 +0330

    Update Student.java

--- exercise1/Student.java
+++ exercise1/Student.java
@@ -3,14 +3,9 @@ package exercise1;
 /**
  * Human is an abstract concept, so we have created an abstract class to represent it.
  * implement or extend the Human class to make a student class.
- * <br>
- * <br>
  * 2.change the student class so that when we call the sayMyName() method on an instance of this class,
  * the fullName attribute of the student is printed.
- * <br>
- * <br>
  * 3.add the following attributes to the student class with setters and getters:
- * <br>
  * studentNumber, majorName, universityName
  */
 public class Student {
